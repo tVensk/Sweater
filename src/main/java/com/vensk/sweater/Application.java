128d7a9ffff83c68d612413d592d2b10806d13ff
Tanya Venskovich <tvenskovich@bk.ru>
2021-11-12 16:52:27 +0300

    Initial commit

---
+++ src/main/java/com/vensk/sweater/Application.java
@@ -0,0 +1,12 @@
+package com.vensk.sweater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
